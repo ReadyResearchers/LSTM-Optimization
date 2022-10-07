7d2119cd765658731375c6f7c79d29e29187535f
colemank <colemank@allegheny.edu>
2022-10-07 11:22:22 -0400

    Reformat test_hyperparam.py

--- hyper_param/tests/test_hyperparam.py
+++ hyper_param/tests/test_hyperparam.py
@@ -2,12 +2,18 @@
 
 from hyper_param import main
 
+
 def test_get_stock_data():
+    """Test the function that extracts the stock data"""
     main.get_stock_data()
 
+
 def test_get_temp_data():
+    """Test the function that extracts the temperature data"""
     main.get_temp_data()
 
+
 def test_objective():
+    """Make sure the objective function works and has non-zero accuracy"""
     study = main.optuna.create_study(study_name="Test", direction="maximize")
     study.optimize(main.objective, n_trials=100, timeout=100)
