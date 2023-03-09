c30f380d5e81c13e62c1bbf01a9f08b9d8b9a6b6
colemank <colemank@allegheny.edu>
2023-03-09 00:07:07 -0500

    Change testing

--- hyper_param/tests/test_hyperparam.py
+++ hyper_param/tests/test_hyperparam.py
@@ -1,16 +1,12 @@
 """Test the artifact's source code"""
 
-from hyper_param import __main__
+from experiment import __main__
 import os
+import subprocess
 import sys
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-def test_objective():
-    """Make sure the objective function works and has non-zero accuracy"""
-    study = __main__.optuna.create_study(study_name="Test", direction="minimize")
-    study.optimize(__main__.objective, n_trials=100, timeout=30)
 
 def test_experiment():
     """Make sure the whole script works as intended"""
-    os.system("python experiment/__main__.py")
+    subprocess.call([sys.executable, '-m', '__main__'])
+    os.system('{} -m __main__'.format(sys.executable))
