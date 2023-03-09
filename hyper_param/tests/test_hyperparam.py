80421b3d8a6e9024af8e5a147474dc5e66762749
colemank <colemank@allegheny.edu>
2023-03-08 23:18:10 -0500

    Implement new test

--- hyper_param/tests/test_hyperparam.py
+++ hyper_param/tests/test_hyperparam.py
@@ -1,14 +1,16 @@
 """Test the artifact's source code"""
 
 from hyper_param import __main__
+import os
+import sys
 
-
-def test_extract_data():
-    """Test the function that extracts the stock data"""
-    __main__.extract_data()
-
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 def test_objective():
     """Make sure the objective function works and has non-zero accuracy"""
     study = __main__.optuna.create_study(study_name="Test", direction="minimize")
-    study.optimize(__main__.objective, n_trials=100, timeout=100)
+    study.optimize(__main__.objective, n_trials=100, timeout=30)
+
+def test_experiment():
+    """Make sure the whole script works as intended"""
+    os.system("python experiment/experiment.py")
