80155543e29ce2e55cff1f3eedba78dcb0e04c7a
colemank <colemank@allegheny.edu>
2023-03-18 00:47:33 -0400

    Comment experiment code

--- hyper_param/experiment/__main__.py
+++ hyper_param/experiment/__main__.py
@@ -9,24 +9,29 @@ if __name__ == "__main__":
 
     sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-    # Controls: 10 epochs, 10 time steps, 10 projection size, 50 trials
+    # Controls: 10 epochs, 10 input and output time steps, 50 trials
 
+    # List of possible values for each command line argument
     epochs = [5, 8, 10, 20, 30]
     time_steps = [3, 8, 10, 15, 20]
     projection_size = [5, 8, 10, 15, 20]
     trials = [10, 25, 50, 75, 100]
 
+    # Initialize the list of mean square errors and optimization algorithms for each permutation
     mse_list = []
     optimizers = []
 
+    # Construction of the collection of subplots for the entire experiment
     data_fig = make_subplots(rows=4, cols=1)
 
+    # Always make the lists hyperparameter values empty when starting a new subplot
     y_batch_size = []
     y_lr = []
     y_layers = []
     y_dropout = []
     y_hidden_size = []
 
+    # Customization of the lines
     trace_batch_size = go.Scatter(x=epochs, y=y_batch_size, name="Batch Size", mode="lines", line=dict(color="blue"))
     trace_lr = go.Scatter(x=epochs, y=y_lr, name="Learning Rate", mode="lines", line=dict(color="red"))
     trace_layers = go.Scatter(x=epochs, y=y_layers, name="Number of Layers", mode="lines", line=dict(color="green"))
@@ -34,10 +39,12 @@ if __name__ == "__main__":
     trace_hidden_size = go.Scatter(x=epochs, y=y_hidden_size, name="Hidden Size", mode="lines", line=dict(color="black"))
 
     for e in epochs:
+        # Initialize each command line argument being passed into LSTM-OP
         program = "hyper_param/lstm.py"
-        csv = "data/delhi_weather_data.csv"
-        input = "[year,month,day,humidity,wind_speed,meanpressure]"
-        output = "[meantemp]"
+        csv = "data/jena_climate_data.csv"
+        input = "[Year,Month,Day,Hour,p..mbar.,rh....,VPmax..mbar.,VPact..mbar.,VPdef..mbar.,sh..g.kg.," \
+                "H2OC..mmol.mol.,rho..g.m..3.,wv..m.s.,max..wv..m.s.,wd..deg.]"
+        output = "[T..degC.]"
         control_time_steps = 10
         control_projection_size = 10
         control_trials = 50
@@ -45,14 +52,19 @@ if __name__ == "__main__":
         command = f"python {program} {csv} {input} {output} {control_time_steps} {control_projection_size}" \
                   f" {control_trials} {test_epochs}"
         print(command)
+
+        # Pass the command into LSTM-OP
         os.system(command)
 
+        # Load the data of the best trial from the pickle file
         with open("experiment/best_values.pickle", "rb") as f:
             best_trial = pickle.load(f)
 
+        # Add to the lists of mean square errors and optimization algorithms
         mse_list.append(best_trial.value)
         optimizers.append(best_trial.params["optimizer"])
 
+        # Add the normalized value of the hyperparameters
         y_batch_size.append((best_trial.params["batch_size"] - 10) / (100 - 10))
         y_lr.append((best_trial.params["lr"] - 0.00001) / (0.01 - 0.00001))
         y_layers.append((best_trial.params["n_layers"] - 2) / (5 - 2))
@@ -60,29 +72,34 @@ if __name__ == "__main__":
         y_hidden_size.append((best_trial.params["hidden_size"] - control_projection_size - 1) /
                              (2048 - control_projection_size - 1))
 
+    # Update the value of the traces
     trace_batch_size.update(y=y_batch_size)
     trace_lr.update(y=y_lr)
     trace_layers.update(y=y_layers)
     trace_dropout.update(y=y_dropout)
     trace_hidden_size.update(y=y_hidden_size)
 
+    # Put those updated values in the subplot
     data_fig.add_trace(trace_batch_size, row=1, col=1)
     data_fig.add_trace(trace_lr, row=1, col=1)
     data_fig.add_trace(trace_layers, row=1, col=1)
     data_fig.add_trace(trace_dropout, row=1, col=1)
     data_fig.add_trace(trace_hidden_size, row=1, col=1)
 
+    # Reset the hyperparameter lists
     y_batch_size = []
     y_lr = []
     y_layers = []
     y_dropout = []
     y_hidden_size = []
 
+    # Same process with the time steps
     for t in time_steps:
         program = "hyper_param/lstm.py"
-        csv = "data/delhi_weather_data.csv"
-        input = "[year,month,day,humidity,wind_speed,meanpressure]"
-        output = "[meantemp]"
+        csv = "data/jena_climate_data.csv"
+        input = "[Year,Month,Day,Hour,p..mbar.,rh....,VPmax..mbar.,VPact..mbar.,VPdef..mbar.,sh..g.kg.," \
+                "H2OC..mmol.mol.,rho..g.m..3.,wv..m.s.,max..wv..m.s.,wd..deg.]"
+        output = "[T..degC.]"
         test_time_steps = t
         control_projection_size = 10
         control_trials = 50
@@ -123,11 +140,13 @@ if __name__ == "__main__":
     y_dropout = []
     y_hidden_size = []
 
+    # Same process with the projection size
     for p in projection_size:
         program = "hyper_param/lstm.py"
-        csv = "data/delhi_weather_data.csv"
-        input = "[year,month,day,humidity,wind_speed,meanpressure]"
-        output = "[meantemp]"
+        csv = "data/jena_climate_data.csv"
+        input = "[Year,Month,Day,Hour,p..mbar.,rh....,VPmax..mbar.,VPact..mbar.,VPdef..mbar.,sh..g.kg.," \
+                "H2OC..mmol.mol.,rho..g.m..3.,wv..m.s.,max..wv..m.s.,wd..deg.]"
+        output = "[T..degC.]"
         control_time_steps = 10
         test_projection_size = p
         control_trials = 50
@@ -167,11 +186,13 @@ if __name__ == "__main__":
     y_dropout = []
     y_hidden_size = []
 
+    # Same process with the trials
     for t in trials:
         program = "hyper_param/lstm.py"
-        csv = "data/delhi_weather_data.csv"
-        input = "[year,month,day,humidity,wind_speed,meanpressure]"
-        output = "[meantemp]"
+        csv = "data/jena_climate_data.csv"
+        input = "[Year,Month,Day,Hour,p..mbar.,rh....,VPmax..mbar.,VPact..mbar.,VPdef..mbar.,sh..g.kg.," \
+                "H2OC..mmol.mol.,rho..g.m..3.,wv..m.s.,max..wv..m.s.,wd..deg.]"
+        output = "[T..degC.]"
         control_time_steps = 10
         control_projection_size = 10
         test_trials = t
@@ -194,10 +215,6 @@ if __name__ == "__main__":
         y_hidden_size.append((best_trial.params["hidden_size"] - control_projection_size - 1) /
                              (2048 - control_projection_size - 1))
 
-
-    print("MSE from all studies:", mse_list)
-    print("Optimizers from all studies:", optimizers)
-
     trace_batch_size.update(x=trials, y=y_batch_size)
     trace_lr.update(x=trials, y=y_lr)
     trace_layers.update(x=trials, y=y_layers)
@@ -210,11 +227,17 @@ if __name__ == "__main__":
     data_fig.add_trace(trace_dropout, row=4, col=1)
     data_fig.add_trace(trace_hidden_size, row=4, col=1)
 
-    data_fig.update_layout(height=1200, width=1200, title="Hyperparameter Trends with Medium Features, "
-                                                          "High Seasonality")
+    # Display the mean square error values and optimization algorithms from all the permutations
+    print("MSE from all studies:", mse_list)
+    print("Optimizers from all studies:", optimizers)
+
+    # Title overall container of subplots and x-axes of those subplots
+    data_fig.update_layout(height=1200, width=1200, title="Hyperparameter Trends with High Features, "
+                                                          "Medium Seasonality")
     data_fig.update_xaxes(title_text="Epochs", row=1, col=1)
     data_fig.update_xaxes(title_text="Input Time Steps", row=2, col=1)
     data_fig.update_xaxes(title_text="Output Time Steps", row=3, col=1)
     data_fig.update_xaxes(title_text="Trials", row=4, col=1)
 
+    # Save the subplot container in an html file
     plot(data_fig, filename="experiment/results.html")
